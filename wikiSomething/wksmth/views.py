e7456ae5c9a7bee59cdd76fdf7adb304487d7e1f
Paul Serafimescu <paulserafimescu@gmail.com>
2020-05-02 15:48:31 -0700

    a something happened v2

--- wikiSomething/wksmth/views.py
+++ wikiSomething/wksmth/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from . models import wikiEntry
+from django.utils import timezone
 
 # Create your views here.
 
 def index(request):
 	entry = get_object_or_404(wikiEntry, pk=1)
 	return render(request, 'test.html', {'search': entry.search, 'search_time': entry.search_date})
+
+def wiki(request):
+	if request.method == 'POST':
+		wikiEntry(search=request.POST, search_date=timezone.now()).save()
+		return HttpResponse('SUPER YO')
+	elif request.method == 'GET':
+		return HttpResponse('YO WTF')
