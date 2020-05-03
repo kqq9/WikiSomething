1a5382d1c0370dcb9dcf30410f95d89c1ca3292c
Zenas Zhu <zenaszhu02@gmail.com>
2020-05-02 17:00:47 -0700

    Search wikipedia and display result
    On POST to /wiki, tries to search Wikipedia for the article requested and display it
    HTML template graciously written by Paul

--- wikiSomething/wksmth/views.py
+++ wikiSomething/wksmth/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from . models import wikiEntry
 from django.utils import timezone
+import wikipedia
 
 # Create your views here.
 
@@ -14,6 +15,18 @@ def wiki(request):
 		new_search = request.POST['index']
 		# Handle request.POST['csrfmiddlewaretoken']
 		wikiEntry(search=new_search, search_date=timezone.now()).save()
-		return HttpResponse('Submitted the search {}'.format(new_search))
+		try:
+			w = wikipedia.summary(new_search)
+			return render(request, 'test2.html', {'arg1': new_search, 'arg2': w})
+		except wikipedia.PageError:
+			try:
+				ww = wikipedia.search(w)
+				if ww:
+					return HttpResponse('No such article {}; found articles:\n{}'.format(new_search, '\n'.join(ww)))
+				else:
+					return HttpResponse('No such article {}; no other results found.'.format(new_search))
+			except:
+				return HttpResponse('No such article {}; no other results found.'.format(new_search))
+		#return HttpResponse('Submitted the search {}'.format(new_search))
 	elif request.method == 'GET':
 		return HttpResponse('Can\'t view this page without submitting a form.')
